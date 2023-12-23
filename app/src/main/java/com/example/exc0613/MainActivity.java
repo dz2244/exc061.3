75583b22b8302c0566a3a222f366ad2d7a083f4e
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-23 20:34:57 +0200

    commit3

--- app/src/main/java/com/example/exc0613/MainActivity.java
+++ app/src/main/java/com/example/exc0613/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.exc0613;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         text369 = findViewById(R.id.text369);    }
     int counter = 0 ;
     public void ace(View view) {
+        text369.setTextColor(Color.BLUE);
         counter += 1 ;
         text369.setText("This is a click number:" + counter);
         if (counter == 6)
